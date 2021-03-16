6bdf8c9e4ad15e66673b0b9bba506795f6bf84a0
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-03-16 11:15:34 +0100

    Deploy to heroku build production

--- server.js
+++ server.js
@@ -75,4 +75,11 @@ router.delete('/todos/:id', (req, res) => {
 
 app.use('/', router);
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
